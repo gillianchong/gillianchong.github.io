d58e9357ddd63e556f550f1a3c02b64c207a547e
Gillian Chong <gillianchong520@gmail.com>
2020-11-02 09:22:32 +0800

    testing2

--- craving-booking-app/scripts/homepage.js
+++ craving-booking-app/scripts/homepage.js
@@ -9,15 +9,15 @@ fetch(url, {
     .then(json => {
       // Do something with object
       console.log(json.homepage);
-      alert(json.username + "successfully added!");
+      alert(json.username + "sign up successfully!");
     });
    
 
    window.addEventListener("load",function(){
     document.getElementById("Confirm").addEventListener("click",function(){
-     let username = document.getElementById("guestName").value;
-     let emailaddress = document.getElementById("guestEmail").value;
-     let password = document.getElementById("guestPassword").value;
+     let username = document.getElementById("username").value;
+     let emailaddress = document.getElementById("emailaddress").value;
+     let password = document.getElementById("password").value;
 
      Confirm(username, emailaddress, password);
     });
