74bc37b884adbb2d5bc284e8e460d082b72b9fa5
Gillian Chong <gillianchong520@gmail.com>
2020-11-03 14:15:48 +0800

    testings

--- craving-booking-app/scripts/booking.js
+++ craving-booking-app/scripts/booking.js
@@ -31,7 +31,7 @@ function BookNow (guestName, guestEmail, guestPhone, guestPax, guestHotel, guest
     });
    }
 
-  window.addEventListener("load",function(){
+  //window.addEventListener("load",function(){
   document.getElementById("BookNow").addEventListener("click",function() {
     let guestname = document.getElementById("guestName").value;
     let guestemail = document.getElementById("guestEmail").value;
